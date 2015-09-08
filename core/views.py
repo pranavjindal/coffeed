3916a1fc70e6d0e88398c1910974d512867de560
Pranav Jindal <pranavjindal@dn51sd2c.sunet>
2015-09-07 19:44:39 -0700

    Added a splash page to the coffeed app!

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+# def TestView(request, **kwargs):
+# 	return HttpResponse("Hello World!")
+
+class SplashView(TemplateView):
+	template_name = "index.html"
